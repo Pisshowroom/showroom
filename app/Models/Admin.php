2a498df7f4f9e760f2a35c459446954f332ab543
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-10-30 07:58:32 +0700

    update 1 file and create 3 files

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens, SoftDeletes;
+
+
 }
