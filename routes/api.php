6634ccf9c37be72a0ed8a5ae87ea4616c06d863c
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-10-30 17:26:39 +0700

    feat: add profile, updateProfile, Logout

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\HomeController;
 use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -38,4 +40,15 @@ Route::group(['prefix' => 'product'], function () {
 Route::group(['prefix' => 'order'], function () {
     Route::post('/precheck', [OrderController::class, 'preCheck']);
     Route::get('/{product}', [ProductController::class, 'show']);
+});
+
+// create route group user
+Route::group(['prefix' => 'user'], function () {
+    Route::post('/login-firebase', [AuthController::class, 'loginFirebase']);
+    // create route group user with middleware auth:api-client
+    Route::middleware('auth:api-client')->group(function () {
+        Route::get('/profile', [UserController::class, 'profile']);
+        Route::post('/update-profile', [UserController::class, 'updateProfile']);
+        Route::post('/logout', [UserController::class, 'logout']);
+    });
 });
