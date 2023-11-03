491f32abc0f4badc19901aa13970e2e0b6207df1
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-11-03 10:36:46 +0700

    feat: Refactor Product & Review resource

---
+++ app/Http/Resources/ReviewResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReviewResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'order_id' => $this->order_id,
+            'user_id' => $this->user_id,
+            'rating' => $this->rating,
+            'text' => $this->text,
+            'images' => $this->images,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+            'user' => new UserResource($this->whenLoaded('user')),
+        ];
+    }
+}
