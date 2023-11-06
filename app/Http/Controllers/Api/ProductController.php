a1ee5bd6fe6129acb5cfb664ca9bc2d47b84e944
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-11-06 13:41:51 +0700

    update ProductController.php

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -55,7 +55,7 @@ class ProductController extends Controller
     public function show(Product $product)
     {
 
-        $product->load(['category', 'variants' => function ($query) {
+        $product->load(['category', 'reviews', 'variants' => function ($query) {
             $query->withoutGlobalScope('hasParentRelation');
         }, 'seller', 'reviews.user']);
 
