2c5eaf6538297c82f7c0cc2b532a121a8882bead
Muhammad Zikry Jasman <zikrybee@gmail.com>
2023-11-08 16:17:34 +0700

    fix

--- config/logging.php
+++ config/logging.php
@@ -75,7 +75,7 @@ return [
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
             'username' => 'Laravel Log',
             'emoji' => ':boom:',
-            'level' => env('LOG_LEVEL', 'critical'),
+            'level' => env('LOG_LEVEL', 'error'),
         ],
 
         'papertrail' => [
