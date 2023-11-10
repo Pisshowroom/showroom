cdbd416ab7049fdbf2e7c39781b48dac79b354d4
Muhammad Zikry Jasman <zikrybee@gmail.com>
2023-11-10 09:37:04 +0700

    if is seller

--- resources/views/clients/buyer/product/detail.blade.php
+++ resources/views/clients/buyer/product/detail.blade.php
@@ -109,7 +109,8 @@
                         </div>
                     </div>
                     <div class="col-lg-7">
-                        <h3 class="color-brand-3 mb-25 line-2" style="overflow-wrap: break-word;">{{ $product->name ?? '' }}</h3>
+                        <h3 class="color-brand-3 mb-25 line-2" style="overflow-wrap: break-word;">
+                            {{ $product->name ?? '' }}</h3>
                         <div class="row align-items-center">
                             <div class="col-lg-4 col-md-4 col-sm-3 mb-mobile"><a
                                     class="byAUthor color-gray-900 font-xs font-medium"
@@ -313,7 +314,8 @@
                                         <tr>
                                             <td>Nama</td>
                                             <td>
-                                                <p class="line-2" style="overflow-wrap: break-word;">{{ $product->name ?? '' }}</p>
+                                                <p class="line-2" style="overflow-wrap: break-word;">
+                                                    {{ $product->name ?? '' }}</p>
                                             </td>
                                         </tr>
                                         <tr>
