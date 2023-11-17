2e9bcf4d2032b16e687872bd2be3d6a5df7c397b
Rudiansyah <rudiansyahether@gmail.com>
2023-11-17 16:51:43 +0700

    update OrderDataController.php and helpers.php

--- app/Http/Controllers/Api/OrderDataController.php
+++ app/Http/Controllers/Api/OrderDataController.php
@@ -11,15 +11,20 @@ class OrderDataController extends Controller
 {
     public function index(Request $request)
     {
+        // validation for request status in Pending, Paid, Sending, Cancel, Returning, Completed
+        $request->validate([
+            'status' => 'in:Pending,Paid,Sending,Cancel,Returning,Completed'
+        ]);
         $user = auth()->guard('api-client')->user();
 
         $status = $request->input('status');
-
+        $status = lypsisGetOrderStatusValues($status);
         // $orders = Order::withCount('order_items')->when($status, function ($query, $status) {
-        $orders = Order::withCount('order_items')->where('user_id', $user->id)->when($status, function ($query, $status) {
-            return $query->where('status', $status);
+        $orders = Order::withCount('order_items')->where('user_id', $user->id)->when($status != null, function ($query) use ($status) {
+            // return $query->where('status', $status);
+            return $query->whereIn('status', $status);
         })
-        ->with(['single_order_item_with_product.product.parent', 'single_order_item_with_product.product.seller'])->paginate(20);
+            ->with(['single_order_item_with_product.product.parent', 'single_order_item_with_product.product.seller'])->paginate(20);
 
         return OrderResource::collection($orders);
     }
@@ -33,14 +38,19 @@ class OrderDataController extends Controller
 
     public function sellerListOrder(Request $request)
     {
+        $request->validate([
+            'status' => 'in:Pending,Paid,Sending,Cancel,Returning,Completed'
+        ]);
         $user = auth()->guard('api-client')->user();
 
         $status = $request->input('status');
+        $status = lypsisGetOrderStatusValues($status);
 
-        $orders = Order::withCount('order_items')->where('seller_id', $user->id)->when($status, function ($query, $status) {
-            return $query->where('status', $status);
+        $orders = Order::withCount('order_items')->where('seller_id', $user->id)->when($status != null, function ($query) use ($status) {
+            // return $query->where('status', $status);
+            return $query->whereIn('status', $status);
         })
-        ->with(['single_order_item_with_product.product.parent', 'single_order_item_with_product.product.seller'])->paginate(20);
+            ->with(['single_order_item_with_product.product.parent', 'single_order_item_with_product.product.seller'])->paginate(20);
 
         return OrderResource::collection($orders);
     }
