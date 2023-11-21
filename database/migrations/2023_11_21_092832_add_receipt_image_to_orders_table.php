e410fb217c3cbd727390cff63c4010fef3dc5f5e
Bima Anzalta <bima77anzalta@gmail.com>
2023-11-21 16:48:36 +0700

    fix

---
+++ database/migrations/2023_11_21_092832_add_receipt_image_to_orders_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->text('receipt_image')->after('delivery_service_kind_description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('receipt_image');
+        });
+    }
+};
