1a817c883383230abd8c3d6e28e3c090901ddeb6
Rudiansyah <rudiansyahether@gmail.com>
2024-01-08 09:32:25 +0700

    update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,17 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('command-orders-packing-due-ended')->twiceDailyAt(13, 14, 38);
+        $schedule->command('command-orders-packing-due-ended')->cron('30 9 * * *');
+        $schedule->command('command-orders-packing-due-ended')->cron('0 10 * * *');
+        $schedule->command('command-orders-packing-due-ended')->cron('30 10 * * *');
+        $schedule->command('command-orders-packing-due-ended')->cron('45 10 * * *');
+        $schedule->command('command-orders-packing-due-ended')->cron('0 11 * * *');
+        $schedule->command('command-orders-packing-due-ended')->cron('20 11 * * *');
+        $schedule->command('command-orders-packing-due-ended')->cron('35 11 * * *');
+        $schedule->command('command-orders-packing-due-ended')->cron('55 11 * * *');
+        $schedule->command('command-orders-packing-due-ended')->cron('10 12 * * *');
+        $schedule->command('command-orders-packing-due-ended')->cron('45 12 * * *');
+        // $schedule->command('command-orders-packing-due-ended')->twiceDailyAt(13, 14, 38);
         // $schedule->command('command-returning-sending-due-ended')->twiceDailyAt(0, 12, 5);
         // $schedule->command('command-payment-due-ended')->twiceDailyAt(0, 12, 10);
         // $schedule->command('command-completed-order-due-ended')->twiceDailyAt(0, 12, 15);
